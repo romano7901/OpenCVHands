0830f55661f84a1a8fb5ef6f8a27d4dc34d651e7
romano <romano@ifiles.ru>
2021-09-16 01:02:28 +0300

    First branch

---
+++ main.py
@@ -0,0 +1,17 @@
+import cv2
+from cvzone.HandTrackingModule import HandDetector
+
+cap = cv2.VideoCapture(0)
+
+cap.set(3,1280)
+cap.set(4,720)
+
+detector = HandDetector(detectionCon=0.8)
+
+while True:
+    success,img = cap.read()
+    img2 = detector.findHands(img)
+   # lmList, bboxInfo = detector.findPosition(img)
+    cv2.imshow('Image',img)
+
+    cv2.waitKey(1)
